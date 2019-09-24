a88b17a4c446f6cec0da8051d001f8cd3e27ab94
Nico De Witte <nico.dewitte@outlook.com>
2019-09-24 12:09:00 +0200

    Refactor test class to be more dry

--- 05_processing_data/ex_rectangle_properties/Tests/UnitTestRectangle.cs
+++ 05_processing_data/ex_rectangle_properties/Tests/UnitTestRectangle.cs
@@ -1,39 +1,34 @@
 using RectangleProperties;
 using System;
+using System.Reflection;
 using Xunit;
 
 namespace Tests
 {
     public class UnitTestRectangle
     {
-        private double GetWidth(Rectangle rectangle)
-        {
-            return (double)typeof(Rectangle)
-                .GetField("width", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                .GetValue(rectangle);
-        }
+        private Rectangle uut = new Rectangle();
 
-        private double GetHeight(Rectangle rectangle)
+        private TReturn GetPrivateField<TReturn>(string fieldname)
         {
-            return (double)typeof(Rectangle)
-                .GetField("height", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                .GetValue(rectangle);
+            Type type = uut.GetType();
+            BindingFlags bindingAttr = BindingFlags.NonPublic | BindingFlags.Instance;
+            FieldInfo field = type.GetField(fieldname, bindingAttr);
+            return (TReturn)field.GetValue(uut);
         }
 
         [Fact]
         public void TestArea()
         {
-            Rectangle rectangle = new Rectangle();
-            Assert.Equal(GetWidth(rectangle) * GetHeight(rectangle),
-                rectangle.CalculateArea());
+            Assert.Equal(GetPrivateField<double>("width") * GetPrivateField<double>("height"),
+                uut.CalculateArea());
         }
 
         [Fact]
         public void TestCircumference()
         {
-            Rectangle rectangle = new Rectangle();
-            Assert.Equal(2*(GetWidth(rectangle) + GetHeight(rectangle)),
-                rectangle.CalculateCircumference());
+            Assert.Equal(2*(GetPrivateField<double>("width") + GetPrivateField<double>("height")),
+                uut.CalculateCircumference());
         }
     }
 }
