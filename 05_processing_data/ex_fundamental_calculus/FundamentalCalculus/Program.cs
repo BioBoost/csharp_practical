dd674e6f1cd896a408653a19df90ec3c9292d48f
Nico De Witte <nico.dewitte@outlook.com>
2019-10-01 15:08:16 +0200

    Fix typo in fundamental calculus

--- 05_processing_data/ex_fundamental_calculus/FundamentalCalculus/Program.cs
+++ 05_processing_data/ex_fundamental_calculus/FundamentalCalculus/Program.cs
@@ -17,7 +17,7 @@ namespace FundamentalCalculus
             Console.WriteLine($"7 - (-2) = {calculator.Subtract(7, -2)}");
 
             Console.WriteLine($"4 * 6 = {calculator.Multiply(4, 6)}");
-            Console.WriteLine($"4 * (-6) = {calculator.Multiply(4, 6)}");
+            Console.WriteLine($"4 * (-6) = {calculator.Multiply(4, -6)}");
 
             // TODO: Add further examples yourself
         }
