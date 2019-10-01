90b07b90dc5a3f923014bbdbd0e453f806fd8cea
Nico De Witte <nico.dewitte@outlook.com>
2019-10-01 10:36:38 +0200

    Remove solution :)

--- 05_processing_data/ex_fundamental_calculus/FundamentalCalculus/Calculator.cs
+++ 05_processing_data/ex_fundamental_calculus/FundamentalCalculus/Calculator.cs
@@ -8,44 +8,44 @@ namespace FundamentalCalculus
     {
         public int Add(int left, int right)
         {
-            // TODO: Return the sum
+            // Example
             return left + right;
         }
 
         public int Subtract(int left, int right)
         {
             // TODO: Return the difference
-            return left - right;
+            return 0;
         }
 
         public int Multiply(int left, int right)
         {
             // TODO: Return the product
-            return left * right;
+            return 0;
         }
 
         public int Divide(int left, int right)
         {
             // TODO: Return the whole divident
-            return left / right;
+            return 0;
         }
 
         public int Modulo(int left, int right)
         {
             // TODO: Return the modulo
-            return left % right;
+            return 0;
         }
 
         public int AbsoluteValue(int value)
         {
             // TODO: Return the absolute value
-            return Math.Abs(value);
+            return 0;
         }
 
         public int Square(int value)
         {
             // TODO: Return the squared value
-            return value * value;
+            return 0;
         }
 
         public int Inverse(int value)
@@ -53,7 +53,7 @@ namespace FundamentalCalculus
             // TODO: Return the inverse value
             // -3 => 3
             // 3 => -3
-            return -value;
+            return 0;
         }
     }
 }
