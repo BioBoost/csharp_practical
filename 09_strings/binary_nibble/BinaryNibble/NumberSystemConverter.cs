20047d7e719a769ce5e5bb71307ee48fd7c19df7
Nico De Witte <nico.dewitte@outlook.com>
2019-10-17 14:10:04 +0200

    Cleanup

--- 09_strings/binary_nibble/BinaryNibble/NumberSystemConverter.cs
+++ 09_strings/binary_nibble/BinaryNibble/NumberSystemConverter.cs
@@ -12,30 +12,6 @@ namespace BinaryNibble
             // binary string representation
             string binary = "";
 
-            int i = 0;
-            while (decimalValue > 0)
-            {
-                if (i == 4) binary = " " + binary;
-
-                if (decimalValue % 2 == 1)
-                {
-                    binary = "1" + binary;
-                }
-                else
-                {
-                    binary = "0" + binary;
-                }
-
-                decimalValue /= 2;
-                i++;
-            }
-
-            for (; i < 8; i++)
-            {
-                if (i == 4) binary = " " + binary;
-                binary = "0" + binary;
-            }
-
             return binary;
         }
     }
